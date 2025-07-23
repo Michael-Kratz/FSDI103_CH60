a5ef8a4015a4f0578082abd4b6a4e6b6b81e49ec
Michael Kratz <aiondeadly@yahoo.com>
2025-07-22 17:06:04 -0700

    Add files via upload

---
+++ FSDI103-CH60/scripts/assignment.js
@@ -0,0 +1,50 @@
+// funny Story
+let character = "Mark";
+let action = "trick";
+let object = "a present";
+let place = "skate park";
+let time = "partly cloudy afternoon";
+
+const occasion = "thses people showed up";
+
+console.log("--- Funny Story ---");
+console.log(character + " was heading to " + place + " on " + time + ".");
+console.log(
+  "As " +
+    character +
+    " was heading down he noticed " +
+    object +
+    " with a note to do a " +
+    action +
+    "."
+);
+console.log(
+  "After doing him trick all suddenly " +
+    occasion +
+    " to till him he won 1 million dollars!"
+);
+
+// a different story
+
+console.log("--- Different Version of the Story ---");
+character = "Mathew";
+action = "flip";
+object = "a fish";
+place = "the lake";
+time = "morning";
+
+console.log(character + " was heading to " + place + " in the " + time + ".");
+console.log(
+  "Suddenly" +
+    character +
+    " seen " +
+    object +
+    " on the ground and did a " +
+    action +
+    "."
+);
+console.log(
+  "After doing him trick all suddenly " +
+    occasion +
+    " to till him he won 1 million dollars!"
+);
---
+++ FSDI103-CH60/scripts/test.js
@@ -0,0 +1,28 @@
+console.log("This is a JS test");
+let ten = 10;
+console.log(ten * ten);
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log("New mood value - ", mood);
+console.log("concatenating the new mood value - ", mood + ", \nthat's all");
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log("Luigi's debt now is", luigisDebt);
+
+let one = 1,
+  two = 2;
+console.log(one + two);
+
+var name = "Stacy";
+const greeting = "Hello";
+console.log(greeting + " " + name);
+
+let character = "Michael";
+let action = "paint";
+let object = "warhammer 40,000";
+let place = "home";
+let time = "in the afternoon";
